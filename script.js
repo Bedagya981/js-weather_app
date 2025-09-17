d1a2667e3c72bceb02cac0935dd10818bb39e52b
Bedagya Raj Khanal <bedagyakhanalraj@gmail.com>
2025-09-17 10:16:25 +0545

    5 day weather forecast using XML and Fetch

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ searchBtn.addEventListener("click", function () {
   const unit = "metric";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
   const xhr = new XMLHttpRequest();
-//   xhr.open("GET", url);
+  xhr.open("GET", url);
   let data;
   xhr.onreadystatechange = function () {
     if (xhr.readyState === 4) {
@@ -29,4 +29,38 @@ searchBtn.addEventListener("click", function () {
     }
   };
   xhr.send()
+
+// 4 days weather data
+const url4day = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${unit}`
+async function checkResponse() {
+  const response = await fetch(url4day)
+  if(response.ok){
+  var data4 = await response.json()
+  let forecast = []
+  let todayDate = new Date().getDate();
+  let tomorrow;
+  for (let i = 0; i < data4.list.length; i++) {
+    const forecastDate = new Date(data4.list[i].dt_txt).getDate()
+    if(forecastDate != todayDate){
+      tomorrow = i;
+      break;
+    }
+  }
+  for (let i = tomorrow; i < data4.list.length; i+=8) {
+    forecast.push(i)
+  }
+  for (const i in forecast) {
+    let lData = data4.list[forecast[i]];
+    let day = new Date(lData.dt_txt)
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let dayName = days[day.getDay()]
+    document.querySelector(`.day${i} .dayE`).innerHTML = dayName
+    document.querySelector(`.day${i} img`).src = `./images/${data4.list[forecast[i]].weather[0].main}.png`
+    document.querySelector(`.day${i} .dayTemp`).innerHTML = `${Math.round(data4.list[forecast[i]].main.temp)}°C`
+  }
+} else{
+  document.querySelector('.error').style.display = "block"
+}
+}
+checkResponse();
 });
