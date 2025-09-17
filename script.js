67d9c8457edee87b6c1c561f16fcf01b72bbd6d4
Bedagya Raj Khanal <bedagyakhanalraj@gmail.com>
2025-09-17 08:18:47 +0545

    Full working single day forecast weather app

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
-const apiKey = "99288e9267d3a7bfbd502136927ca0d6"
-const searchField = document.querySelector(".search input")
-const searchBtn = document.querySelector(".search button")
-let city
-searchBtn.addEventListener('click', function(){
-    city = searchField.value
-})
-const unit = ""
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`
-const xhr = new XMLHttpRequest();
-// xhr.open('GET', url);
-let data
-xhr.onreadystatechange = function(){
-    console.log(xhr.readyState);
-    if(xhr.readyState === 4){
-        data = JSON.parse(this.responseText)
-        document.querySelector(".city").innerHTML = data.name
-        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C"
-        document.querySelector(".humidity").innerHTML = data.main.humidity + "%"
-        document.querySelector(".wind").innerHTML = data.wind.speed + "km/h"
-        document.querySelector(".weather-icon").src = `./images/${data.weather[0].main}.png`
-    }else{
-        document.querySelector(".error").style.display = "block"
-        document.querySelector(".weather").style.display = "none"
+const searchBtn = document.querySelector(".search button");
+searchBtn.addEventListener("click", function () {
+  const apiKey = "99288e9267d3a7bfbd502136927ca0d6";
+  const searchField = document.querySelector(".search input");
+  const city = searchField.value;
+  const unit = "metric";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+  const xhr = new XMLHttpRequest();
+//   xhr.open("GET", url);
+  let data;
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        document.querySelector(".weather").style.display = "block";
+        data = JSON.parse(this.responseText);
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML =
+          Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML =
+          data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+        document.querySelector(
+          ".weather-icon"
+        ).src = `./images/${data.weather[0].main}.png`;
+      } else {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+      }
     }
-}
+  };
+  xhr.send()
+});
